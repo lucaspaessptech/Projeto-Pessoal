5c584ee30d89b18fc8361cc5cca4c761779b6234
lucaspaessptech <lucas.paes@sptech.school>
2024-11-21 17:37:41 -0300

    Adição de páginas no banco de dados e ajuste no html das páginas

--- Site e Api/src/models/questaoModel.js
+++ Site e Api/src/models/questaoModel.js
@@ -1,8 +1,7 @@
 var database = require("../database/config")
 
 function carregarConteudo(id){
-    console.log(id)
-    var instrucaoSql = `SELECT * from topico where idTopico = ${id};`
+    var instrucaoSql = `SELECT * from topico left join questao on idTopico = fkTopico where idTopico = ${id};`
     return database.executar(instrucaoSql);
 }
 
