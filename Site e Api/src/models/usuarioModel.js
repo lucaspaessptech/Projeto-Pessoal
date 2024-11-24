b03fb3b986a7b4619587e73b33b12a3c8d083291
lucaspaessptech <lucas.paes@sptech.school>
2024-11-24 19:45:42 -0300

    Mudanças da página inicial e adição de criptografia na senha

--- Site e Api/src/models/usuarioModel.js
+++ Site e Api/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT email, senha FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT email, senha FROM usuario WHERE email = '${email}' AND senha = SHA2('${senha}', 256);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -16,7 +16,7 @@ function cadastrar(usuario, email, senha) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha) VALUES ('${usuario}', '${email}', '${senha}');
+        INSERT INTO usuario (nome, email, senha) VALUES ('${usuario}', '${email}', SHA2('${senha}', 256));
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
